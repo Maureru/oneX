a6cd7d8100d3ee4c715e2fde3138dd53e82b5d31
xMauu <102646969+xMauu@users.noreply.github.com>
2024-02-04 06:50:52 +0800

    y

--- data/data.js
+++ data/data.js
@@ -4,6 +4,10 @@ export const links = {
         name: "1XBET High Odds",
         link: "https://refpa4293501.top/L?tag=d_3007175m_1622c_&site=3007175&ad=1622&r=Registration/"
     },
+    "onexx": {
+        name: "1XBET High Odds",
+        link: "https://cutt.ly/awXnV7AK"
+    },
     "gamex": {
         name: "GAMEX Number One Online Ending",
         link: "https://gamexsports.com.ph/vNG9y/lygzW/yLRzN"
