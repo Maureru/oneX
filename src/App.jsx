ea3b48853377a64fac3c6022df65e5613e4f8c8d
xMauu <102646969+xMauu@users.noreply.github.com>
2024-02-04 06:48:03 +0800

    wt

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='txt'>
       <h1>OneX Basketball</h1>
-      <h2>Register to 1xBet Here</h2>
+      <h3>Register to 1xBet Here</h3>
       <a className='btn' href={links.onex.link}>Register Now</a>
       <h4>PROMO CODE: <span>TAMBAYENGINEER</span></h4>
     </div>
