e257d75ea04f1da377a1368494f17dd4ab78cfa2
xMauu <102646969+xMauu@users.noreply.github.com>
2024-02-17 09:11:45 +0800

    updates two

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
     <div className='txt'>
       <h1>OneX Philippines</h1>
       <h3>Register to 1xBet Here</h3>
-      <a className='btn' href={links.onex.link}>Register Now</a>
+      <a className='btn' href={links.onexx.link}>Register Now</a>
       <h4>PROMO CODE: <span>TAMBAYENGINEER</span></h4>
       <h5 style={{
         textTransform: "uppercase"
-      }}>For Guide and Assistance. Join Our</h5>
+      }}>For Guide and Assistance. Join Our GC</h5>
       <a style={{
         padding: "5px 10px",
         border: "2px solid #535bf2",
