886f7dc28df5d7719cfd41b6a56c1ae4f80b5ae1
xMauu <102646969+xMauu@users.noreply.github.com>
2024-02-17 08:09:57 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import './App.css'
 function App() {
   return (
     <div className='txt'>
-      <h1>OneX Basketball</h1>
+      <h1>OneX Philippines</h1>
       <h3>Register to 1xBet Here</h3>
       <a className='btn' href={links.onexx.link}>Register Now</a>
       <h4>PROMO CODE: <span>TAMBAYENGINEER</span></h4>
+      <h5 style={{
+        textTransform: "uppercase"
+      }}>For Guide and Assistance. Join Our</h5>
+      <a style={{
+        padding: "5px 10px",
+        border: "2px solid #535bf2",
+        borderRadius: "5px"
+      }} href="https://m.me/j/AbZ3ttNWB3SVVeyO/">Player's Discussion VIP Group</a>
+      <h3 className='macoo'>@Macoo - Official Agent</h3>
     </div>
   )
 }
